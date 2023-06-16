e10e998c1ab3aa834309224d17d34a8a3f8cab01
aymen ghebriou <aymen.ghebriou3@gmail.com>
2023-06-16 21:57:16 +0100

    i have just added few comments to my javascript code to make it clear

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// a table to store the quotes (content & author).
 const quotes = [
     {
       content: "You are the sun, and I'm the moon. In your shadow I can shine.",
@@ -17,47 +18,49 @@ const quotes = [
     }
   ];
   
+  //it's an index for the quotes table ( starting from zero ).
   var currentQuoteIndex = 0;
   
+  //it's a function used to generate a quote in our webpage when the user click on Generate Quote btn.
   function generateQuote() {
     var quoteContent = document.getElementById('quote-content');
     var quoteAuthor = document.getElementById('quote-author');
     
+    //turn the quote index to zero if it's bigger than the size of quotes table.
     if (currentQuoteIndex >= quotes.length) {
       currentQuoteIndex = 0;
     }
-    
+    //declare a quote var and initialise it to an element of qoutes table by an index.
     var quote = quotes[currentQuoteIndex];
-    quoteContent.textContent = quote.content;
+    quoteContent.textContent = quote.content; 
     quoteAuthor.textContent = quote.author;
     
-    currentQuoteIndex++;
+    currentQuoteIndex++; //increment the index on each time.
   }
-   
+   // a function to mark a quote into your favorite list.
   function markAsFavorite() {
-var quoteContent = document.getElementById('quote-content');
-var quoteAuthor = document.getElementById('quote-author');
-var favoriteQuotesList = document.getElementById('favorite-quotes-list');
-
-if (quoteContent.textContent !== "" && quoteAuthor.textContent !== "") {
-  var favoriteQuote = quoteContent.textContent + ' - ' + quoteAuthor.textContent;
+    var quoteContent = document.getElementById('quote-content');
+    var quoteAuthor = document.getElementById('quote-author');
+    var favoriteQuotesList = document.getElementById('favorite-quotes-list');
+    //check if the user has already generated a quote,if it's empty nothing will happen
+    if (quoteContent.textContent !== "" && quoteAuthor.textContent !== "") {
+      var favoriteQuote = quoteContent.textContent + ' - ' + quoteAuthor.textContent;
   
-  // Check if the quote is already in the favorite quotes list
-  var existingQuotes = favoriteQuotesList.getElementsByTagName('li');
-  var isQuoteAlreadyFavorite = false;
+    // Check if the quote is already in the favorite quotes list
+    var existingQuotes = favoriteQuotesList.getElementsByTagName('li');
+    var isQuoteAlreadyFavorite = false;
   
-  for (var i = 0; i < existingQuotes.length; i++) {
-    if (existingQuotes[i].textContent === favoriteQuote) {
-      isQuoteAlreadyFavorite = true;
-      break;
+    for (var i = 0; i < existingQuotes.length; i++) {
+      if (existingQuotes[i].textContent === favoriteQuote) {
+        isQuoteAlreadyFavorite = true;
+        break;
+      }
+    }
+    // add the quote to ur FV list if it doesn't exists 
+    if (!isQuoteAlreadyFavorite) {
+     var listItem = document.createElement('li'); //create a list item to add it to ur list.
+     listItem.textContent = favoriteQuote;
+     favoriteQuotesList.appendChild(listItem);
     }
   }
-  
-  if (!isQuoteAlreadyFavorite) {
-    var listItem = document.createElement('li');
-    listItem.textContent = favoriteQuote;
-  
-    favoriteQuotesList.appendChild(listItem);
   }
-}
-}
